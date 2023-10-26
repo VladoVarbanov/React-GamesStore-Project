8598ab21eb77ca424469269bf209113939e615da
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-10-26 20:11:46 +0300

    Header JSX Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Header from "./components/Header.jsx";
+
 function App() {
   return (
     <>
@@ -16,49 +18,7 @@ function App() {
       {/* <!-- ***** Preloader End ***** --> */}
 
       {/* <!-- ***** Header Area Start ***** --> */}
-      <header className="header-area header-sticky">
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <nav className="main-nav">
-                {/* <!-- ***** Logo Start ***** --> */}
-                <a href="index.html" className="logo">
-                  <img
-                    src="/images/logo.png"
-                    alt=""
-                    style={{ width: "158px" }}
-                  />
-                </a>
-                {/* <!-- ***** Logo End ***** --> */}
-                {/* <!-- ***** Menu Start ***** --> */}
-                <ul className="nav">
-                  <li>
-                    <a href="index.html" className="active">
-                      Home
-                    </a>
-                  </li>
-                  <li>
-                    <a href="shop.html">Our Shop</a>
-                  </li>
-                  <li>
-                    <a href="product-details.html">Product Details</a>
-                  </li>
-                  <li>
-                    <a href="contact.html">Contact Us</a>
-                  </li>
-                  <li>
-                    <a href="#">Sign In</a>
-                  </li>
-                </ul>
-                <a className="menu-trigger">
-                  <span>Menu</span>
-                </a>
-                {/* <!-- ***** Menu End ***** --> */}
-              </nav>
-            </div>
-          </div>
-        </div>
-      </header>
+      <Header />
       {/* <!-- ***** Header Area End ***** --> */}
 
       <div className="main-banner">
---
+++ src/components/Header.jsx
@@ -0,0 +1,43 @@
+export default function Header(props) {
+  return (
+    <header className="header-area header-sticky">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <nav className="main-nav">
+              {/* <!-- ***** Logo Start ***** --> */}
+              <a href="index.html" className="logo">
+                <img src="/images/logo.png" alt="" style={{ width: "158px" }} />
+              </a>
+              {/* <!-- ***** Logo End ***** --> */}
+              {/* <!-- ***** Menu Start ***** --> */}
+              <ul className="nav">
+                <li>
+                  <a href="index.html" className="active">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="shop.html">Our Shop</a>
+                </li>
+                <li>
+                  <a href="product-details.html">Product Details</a>
+                </li>
+                <li>
+                  <a href="contact.html">Contact Us</a>
+                </li>
+                <li>
+                  <a href="#">Sign In</a>
+                </li>
+              </ul>
+              <a className="menu-trigger">
+                <span>Menu</span>
+              </a>
+              {/* <!-- ***** Menu End ***** --> */}
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
