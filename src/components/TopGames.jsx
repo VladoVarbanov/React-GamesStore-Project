bf535baac11593d23032c034bd19e0d364bb3419
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-10-26 20:21:23 +0300

    Create TopGames JSX component

---
+++ src/components/TopGames.jsx
@@ -0,0 +1,105 @@
+export default function TopGames(props) {
+  return (
+    <div className="section most-played">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="section-heading">
+              <h6>TOP GAMES</h6>
+              <h2>Most Played</h2>
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="main-button">
+              <a href="shop.html">View All</a>
+            </div>
+          </div>
+          <div className="col-lg-2 col-md-6 col-sm-6">
+            <div className="item">
+              <div className="thumb">
+                <a href="product-details.html">
+                  <img src="/images/top-game-01.jpg" alt="" />
+                </a>
+              </div>
+              <div className="down-content">
+                <span className="category">Adventure</span>
+                <h4>Assasin Creed</h4>
+                <a href="product-details.html">Explore</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-2 col-md-6 col-sm-6">
+            <div className="item">
+              <div className="thumb">
+                <a href="product-details.html">
+                  <img src="/images/top-game-02.jpg" alt="" />
+                </a>
+              </div>
+              <div className="down-content">
+                <span className="category">Adventure</span>
+                <h4>Assasin Creed</h4>
+                <a href="product-details.html">Explore</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-2 col-md-6 col-sm-6">
+            <div className="item">
+              <div className="thumb">
+                <a href="product-details.html">
+                  <img src="/images/top-game-03.jpg" alt="" />
+                </a>
+              </div>
+              <div className="down-content">
+                <span className="category">Adventure</span>
+                <h4>Assasin Creed</h4>
+                <a href="product-details.html">Explore</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-2 col-md-6 col-sm-6">
+            <div className="item">
+              <div className="thumb">
+                <a href="product-details.html">
+                  <img src="/images/top-game-04.jpg" alt="" />
+                </a>
+              </div>
+              <div className="down-content">
+                <span className="category">Adventure</span>
+                <h4>Assasin Creed</h4>
+                <a href="product-details.html">Explore</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-2 col-md-6 col-sm-6">
+            <div className="item">
+              <div className="thumb">
+                <a href="product-details.html">
+                  <img src="/images/top-game-05.jpg" alt="" />
+                </a>
+              </div>
+              <div className="down-content">
+                <span className="category">Adventure</span>
+                <h4>Assasin Creed</h4>
+                <a href="product-details.html">Explore</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-2 col-md-6 col-sm-6">
+            <div className="item">
+              <div className="thumb">
+                <a href="product-details.html">
+                  <img src="/images/top-game-06.jpg" alt="" />
+                </a>
+              </div>
+              <div className="down-content">
+                <span className="category">Adventure</span>
+                <h4>Assasin Creed</h4>
+                <a href="product-details.html">Explore</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
