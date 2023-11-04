96bb826ace4e5133da586db133293c1345b11913
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-11-04 11:45:31 +0200

    rename OurShopPage to CatalogPage

---
+++ src/components/pages/CatalogPage.jsx
@@ -0,0 +1,28 @@
+import Footer from "../Footer.jsx";
+import NavBar from "../NavBar.jsx";
+import ShopStocks from "../ShopStocks.jsx";
+
+export default function CatalogPage(props) {
+  return (
+    <>
+      <NavBar />
+
+      <div className="page-heading header-text">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <h3>Our Shop</h3>
+              <span className="breadcrumb">
+                <a href="/">Home</a> {">"} Our Shop
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <ShopStocks />
+
+      <Footer />
+    </>
+  );
+}
