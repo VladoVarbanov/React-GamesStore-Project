81a429c8eb90d5619359fef16a5a4ecc84a7f585
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-11-04 11:44:48 +0200

    Update MainPage

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -2,7 +2,7 @@ import CallToAction from "../CallToAction";
 import Categories from "../Categories.jsx";
 import Footer from "../Footer.jsx";
 import Futures from "../Futures.jsx";
-import Header from "../Header.jsx";
+import NavBar from "../NavBar.jsx";
 import MainBanner from "../MainBanner.jsx";
 import TopGames from "../TopGames.jsx";
 import Trending from "../Trending.jsx";
@@ -11,7 +11,7 @@ export default function MainPage(props) {
   return (
     <>
       {/* <!-- ***** Header Area Start ***** --> */}
-      <Header />
+      <NavBar />
       {/* <!-- ***** Header Area End ***** --> */}
 
       <MainBanner />
