03efd237ef77b0ecbfdc4e1ef8e54df8ee6e49bf
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-11-04 11:47:52 +0200

    Update ShopStock component

--- src/components/ShopStocks.jsx
+++ src/components/ShopStocks.jsx
@@ -1,10 +1,10 @@
 export default function ShopStocks(props) {
   return (
-    <div class="section trending">
-      <div class="container">
-        <ul class="trending-filter">
+    <div className="section trending">
+      <div className="container">
+        <ul className="trending-filter">
           <li>
-            <a class="is_active" href="#!" data-filter="*">
+            <a className="is_active" href="#!" data-filter="*">
               Show All
             </a>
           </li>
@@ -24,239 +24,239 @@ export default function ShopStocks(props) {
             </a>
           </li>
         </ul>
-        <div class="row trending-box">
-          <div class="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 adv">
-            <div class="item">
-              <div class="thumb">
+        <div className="row trending-box">
+          <div className="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 adv">
+            <div className="item">
+              <div className="thumb">
                 <a href="product-details.html">
-                  <img src="assets/images/trending-01.jpg" alt="" />
+                  <img src="/images/trending-01.jpg" alt="" />
                 </a>
-                <span class="price">
+                <span className="price">
                   <em>$36</em>$24
                 </span>
               </div>
-              <div class="down-content">
-                <span class="category">Action</span>
+              <div className="down-content">
+                <span className="category">Action</span>
                 <h4>Assasin Creed</h4>
                 <a href="product-details.html">
-                  <i class="fa fa-shopping-bag"></i>
+                  <i className="fa fa-shopping-bag"></i>
                 </a>
               </div>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 str">
-            <div class="item">
-              <div class="thumb">
+          <div className="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 str">
+            <div className="item">
+              <div className="thumb">
                 <a href="product-details.html">
-                  <img src="assets/images/trending-02.jpg" alt="" />
+                  <img src="/images/trending-02.jpg" alt="" />
                 </a>
-                <span class="price">
+                <span className="price">
                   <em>$32</em>$22
                 </span>
               </div>
-              <div class="down-content">
-                <span class="category">Action</span>
+              <div className="down-content">
+                <span className="category">Action</span>
                 <h4>Assasin Creed</h4>
                 <a href="product-details.html">
-                  <i class="fa fa-shopping-bag"></i>
+                  <i className="fa fa-shopping-bag"></i>
                 </a>
               </div>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 adv rac">
-            <div class="item">
-              <div class="thumb">
+          <div className="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 adv rac">
+            <div className="item">
+              <div className="thumb">
                 <a href="product-details.html">
-                  <img src="assets/images/trending-03.jpg" alt="" />
+                  <img src="/images/trending-03.jpg" alt="" />
                 </a>
-                <span class="price">
+                <span className="price">
                   <em>$45</em>$30
                 </span>
               </div>
-              <div class="down-content">
-                <span class="category">Action</span>
+              <div className="down-content">
+                <span className="category">Action</span>
                 <h4>Assasin Creed</h4>
                 <a href="product-details.html">
-                  <i class="fa fa-shopping-bag"></i>
+                  <i className="fa fa-shopping-bag"></i>
                 </a>
               </div>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 str">
-            <div class="item">
-              <div class="thumb">
+          <div className="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 str">
+            <div className="item">
+              <div className="thumb">
                 <a href="product-details.html">
-                  <img src="assets/images/trending-04.jpg" alt="" />
+                  <img src="/images/trending-04.jpg" alt="" />
                 </a>
-                <span class="price">
+                <span className="price">
                   <em>$32</em>$22
                 </span>
               </div>
-              <div class="down-content">
-                <span class="category">Action</span>
+              <div className="down-content">
+                <span className="category">Action</span>
                 <h4>Assasin Creed</h4>
                 <a href="product-details.html">
-                  <i class="fa fa-shopping-bag"></i>
+                  <i className="fa fa-shopping-bag"></i>
                 </a>
               </div>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 rac str">
-            <div class="item">
-              <div class="thumb">
+          <div className="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 rac str">
+            <div className="item">
+              <div className="thumb">
                 <a href="product-details.html">
-                  <img src="assets/images/trending-03.jpg" alt="" />
+                  <img src="/images/trending-03.jpg" alt="" />
                 </a>
-                <span class="price">
+                <span className="price">
                   <em>$38</em>$26
                 </span>
               </div>
-              <div class="down-content">
-                <span class="category">Action</span>
+              <div className="down-content">
+                <span className="category">Action</span>
                 <h4>Assasin Creed</h4>
                 <a href="product-details.html">
-                  <i class="fa fa-shopping-bag"></i>
+                  <i className="fa fa-shopping-bag"></i>
                 </a>
               </div>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 rac adv">
-            <div class="item">
-              <div class="thumb">
+          <div className="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 rac adv">
+            <div className="item">
+              <div className="thumb">
                 <a href="product-details.html">
-                  <img src="assets/images/trending-01.jpg" alt="" />
+                  <img src="/images/trending-01.jpg" alt="" />
                 </a>
-                <span class="price">
+                <span className="price">
                   <em>$30</em>$20
                 </span>
               </div>
-              <div class="down-content">
-                <span class="category">Action</span>
+              <div className="down-content">
+                <span className="category">Action</span>
                 <h4>Assasin Creed</h4>
                 <a href="product-details.html">
-                  <i class="fa fa-shopping-bag"></i>
+                  <i className="fa fa-shopping-bag"></i>
                 </a>
               </div>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 rac str">
-            <div class="item">
-              <div class="thumb">
+          <div className="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 rac str">
+            <div className="item">
+              <div className="thumb">
                 <a href="product-details.html">
-                  <img src="assets/images/trending-04.jpg" alt="" />
+                  <img src="/images/trending-04.jpg" alt="" />
                 </a>
-                <span class="price">
+                <span className="price">
                   <em>$32</em>$22
                 </span>
               </div>
-              <div class="down-content">
-                <span class="category">Action</span>
+              <div className="down-content">
+                <span className="category">Action</span>
                 <h4>Assasin Creed</h4>
                 <a href="product-details.html">
-                  <i class="fa fa-shopping-bag"></i>
+                  <i className="fa fa-shopping-bag"></i>
                 </a>
               </div>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 rac adv">
-            <div class="item">
-              <div class="thumb">
+          <div className="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 rac adv">
+            <div className="item">
+              <div className="thumb">
                 <a href="product-details.html">
-                  <img src="assets/images/trending-02.jpg" alt="" />
+                  <img src="/images/trending-02.jpg" alt="" />
                 </a>
-                <span class="price">
+                <span className="price">
                   <em>$32</em>$22
                 </span>
               </div>
-              <div class="down-content">
-                <span class="category">Action</span>
+              <div className="down-content">
+                <span className="category">Action</span>
                 <h4>Assasin Creed</h4>
                 <a href="product-details.html">
-                  <i class="fa fa-shopping-bag"></i>
+                  <i className="fa fa-shopping-bag"></i>
                 </a>
               </div>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 adv rac">
-            <div class="item">
-              <div class="thumb">
+          <div className="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 adv rac">
+            <div className="item">
+              <div className="thumb">
                 <a href="product-details.html">
-                  <img src="assets/images/trending-03.jpg" alt="" />
+                  <img src="/images/trending-03.jpg" alt="" />
                 </a>
-                <span class="price">
+                <span className="price">
                   <em>$28</em>$20
                 </span>
               </div>
-              <div class="down-content">
-                <span class="category">Action</span>
+              <div className="down-content">
+                <span className="category">Action</span>
                 <h4>Assasin Creed</h4>
                 <a href="product-details.html">
-                  <i class="fa fa-shopping-bag"></i>
+                  <i className="fa fa-shopping-bag"></i>
                 </a>
               </div>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 str">
-            <div class="item">
-              <div class="thumb">
+          <div className="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 str">
+            <div className="item">
+              <div className="thumb">
                 <a href="product-details.html">
-                  <img src="assets/images/trending-04.jpg" alt="" />
+                  <img src="/images/trending-04.jpg" alt="" />
                 </a>
-                <span class="price">
+                <span className="price">
                   <em>$26</em>$18
                 </span>
               </div>
-              <div class="down-content">
-                <span class="category">Action</span>
+              <div className="down-content">
+                <span className="category">Action</span>
                 <h4>Assasin Creed</h4>
                 <a href="product-details.html">
-                  <i class="fa fa-shopping-bag"></i>
+                  <i className="fa fa-shopping-bag"></i>
                 </a>
               </div>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 adv">
-            <div class="item">
-              <div class="thumb">
+          <div className="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 adv">
+            <div className="item">
+              <div className="thumb">
                 <a href="product-details.html">
-                  <img src="assets/images/trending-01.jpg" alt="" />
+                  <img src="/images/trending-01.jpg" alt="" />
                 </a>
-                <span class="price">
+                <span className="price">
                   <em>$32</em>$24
                 </span>
               </div>
-              <div class="down-content">
-                <span class="category">Action</span>
+              <div className="down-content">
+                <span className="category">Action</span>
                 <h4>Assasin Creed</h4>
                 <a href="product-details.html">
-                  <i class="fa fa-shopping-bag"></i>
+                  <i className="fa fa-shopping-bag"></i>
                 </a>
               </div>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 str">
-            <div class="item">
-              <div class="thumb">
+          <div className="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 str">
+            <div className="item">
+              <div className="thumb">
                 <a href="product-details.html">
-                  <img src="assets/images/trending-02.jpg" alt="" />
+                  <img src="/images/trending-02.jpg" alt="" />
                 </a>
-                <span class="price">
+                <span className="price">
                   <em>$45</em>$30
                 </span>
               </div>
-              <div class="down-content">
-                <span class="category">Action</span>
+              <div className="down-content">
+                <span className="category">Action</span>
                 <h4>Assasin Creed</h4>
                 <a href="product-details.html">
-                  <i class="fa fa-shopping-bag"></i>
+                  <i className="fa fa-shopping-bag"></i>
                 </a>
               </div>
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-lg-12">
-            <ul class="pagination">
+        <div className="row">
+          <div className="col-lg-12">
+            <ul className="pagination">
               <li>
                 <a href="#"> &lt; </a>
               </li>
@@ -264,7 +264,7 @@ export default function ShopStocks(props) {
                 <a href="#">1</a>
               </li>
               <li>
-                <a class="is_active" href="#">
+                <a className="is_active" href="#">
                   2
                 </a>
               </li>
