89515509898753fba16337d89a0108eec624b55f
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-11-04 10:35:39 +0200

    Setup react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-import CallToAction from "./components/CallToAction.jsx";
-import Categories from "./components/Categories.jsx";
-import Footer from "./components/Footer.jsx";
-import Futures from "./components/Futures.jsx";
-import Header from "./components/Header.jsx";
-import MainBanner from "./components/MainBanner.jsx";
-import TopGames from "./components/TopGames.jsx";
-import Trending from "./components/Trending.jsx";
+import { Route, Routes } from "react-router-dom";
+
+import MainPage from "./components/pages/MainPage.jsx";
 
 function App() {
   return (
@@ -23,24 +18,9 @@ function App() {
         </div>
       </div>
       {/* <!-- ***** Preloader End ***** --> */}
-
-      {/* <!-- ***** Header Area Start ***** --> */}
-      <Header />
-      {/* <!-- ***** Header Area End ***** --> */}
-
-      <MainBanner />
-
-      <Futures />
-
-      <Trending />
-
-      <TopGames />
-
-      <Categories />
-
-      <CallToAction />
-
-      <Footer />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
     </>
   );
 }
