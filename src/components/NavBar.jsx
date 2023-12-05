a6f6474873744d8b48911b4728634e37a3e61865
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-12-05 21:25:21 +0200

    create Navbar button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { logOut } from "../services/firebaseGamesDB.jsx";
 import { currentUser } from "../services/firebaseGamesDB.jsx";
+import "../../public/css/dropDownMenu.css";
 
 export default function Header({ activePage }) {
   let page = "";
   const [activeTab, setActiveTab] = useState(page);
+  const [showMenu, setShowMenu] = useState(false);
   if (activePage === "home") {
     page = "home";
   } else if (activePage === "catalog") {
@@ -24,6 +26,10 @@ export default function Header({ activePage }) {
     logOut();
   };
   const user = currentUser();
+  const toggleMenu = () => {
+    console.log("Toggle menu On");
+    setShowMenu(!showMenu);
+  };
   return (
     <header className="header-area header-sticky">
       <div className="container">
@@ -31,9 +37,7 @@ export default function Header({ activePage }) {
           <div className="col-12">
             <nav className="main-nav">
               {/* <!-- ***** Logo Start ***** --> */}
-              <Link to="/" className="logo">
-                <img src="/images/logo.png" alt="" style={{ width: "158px" }} />
-              </Link>
+
               {/* <!-- ***** Logo End ***** --> */}
               {/* <!-- ***** Menu Start ***** --> */}
               <ul className="nav">
@@ -113,9 +117,88 @@ export default function Header({ activePage }) {
                   </li>
                 )}
               </ul>
-              <a className="menu-trigger">
+              <Link className="menu-trigger" onClick={toggleMenu}>
                 <span>Menu</span>
-              </a>
+              </Link>
+              {showMenu && (
+                <div className="dropdown-menu">
+                  <li>
+                    <Link
+                      to="/"
+                      className={activeTab === "home" ? "active" : ""}
+                      onClick={() => handleTabClick("home")}
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/catalog"
+                      className={activeTab === "catalog" ? "active" : ""}
+                      onClick={() => handleTabClick("catalog")}
+                    >
+                      Game Catalog
+                    </Link>
+                  </li>
+                  {user ? (
+                    <li>
+                      <Link
+                        to="/sell"
+                        className={activeTab === "sell" ? "active" : ""}
+                        onClick={() => handleTabClick("sell")}
+                      >
+                        Sell Game
+                      </Link>
+                    </li>
+                  ) : (
+                    ""
+                  )}
+
+                  {activePage === "details" && (
+                    <li>
+                      <Link
+                        to="/game-details"
+                        className={activeTab === "details" ? "active" : ""}
+                        onClick={() => handleTabClick("details")}
+                      >
+                        Game Details
+                      </Link>
+                    </li>
+                  )}
+
+                  {/* <li>
+                  <a href="game-details.html">Meet the Indies</a>
+                </li> */}
+
+                  {/* Contact Us */}
+                  {/* <li>
+                  <a href="contact.html">Contact Us</a>
+                </li> */}
+
+                  {user ? (
+                    <li>
+                      <Link>{user.displayName}</Link>
+                    </li>
+                  ) : (
+                    ""
+                  )}
+                  {user === "" ? (
+                    <li>
+                      <Link
+                        to="/login"
+                        className={activeTab === "details" ? "active" : ""}
+                        onClick={() => handleTabClick("details")}
+                      >
+                        Sign In
+                      </Link>
+                    </li>
+                  ) : (
+                    <li>
+                      <Link onClick={signOut}>SIGN OUT</Link>
+                    </li>
+                  )}
+                </div>
+              )}
               {/* <!-- ***** Menu End ***** --> */}
             </nav>
           </div>
